aee9dc2b206e5d025c49c5bc4563e09102bd95c5
Jake Lee <github@jakelee.co.uk>
2019-05-14 19:51:19 +0100

    Placeholder activity

---
+++ app/src/main/java/uk/co/jakelee/daggerexperiments/proandroiddev/ProAndroidDevActivity.kt
@@ -0,0 +1,13 @@
+package uk.co.jakelee.daggerexperiments.proandroiddev
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import uk.co.jakelee.daggerexperiments.R
+
+class ProAndroidDevActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pro_android_dev)
+    }
+}
