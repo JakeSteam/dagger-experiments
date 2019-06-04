c7d6d4f67cfa9731164eead2521058c4acf29fc4
Jake Lee <github@jakelee.co.uk>
2019-06-04 13:33:48 +0100

    Naming infos

--- app/src/main/java/uk/co/jakelee/daggerexperiments/elyeproject/Bag.kt
+++ app/src/main/java/uk/co/jakelee/daggerexperiments/elyeproject/Bag.kt
@@ -2,11 +2,17 @@ package uk.co.jakelee.daggerexperiments.elyeproject
 
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 
 @Module
 class Bag {
-    @Provides
+    @Provides @Named(LOVE)
     open fun sayLoveDagger2(): Info {
         return Info("Love Dagger 2")
+    }
+
+    @Provides @Named(HELLO)
+    open fun sayHelloDagger2(): Info {
+        return Info("Hello Dagger 2")
     }
 }
--- app/src/main/java/uk/co/jakelee/daggerexperiments/elyeproject/ElyeProjectActivity.kt
+++ app/src/main/java/uk/co/jakelee/daggerexperiments/elyeproject/ElyeProjectActivity.kt
@@ -6,16 +6,21 @@ import dagger.Component
 import kotlinx.android.synthetic.main.activity_elye_project.*
 import uk.co.jakelee.daggerexperiments.R
 import javax.inject.Inject
+import javax.inject.Named
+
+const val LOVE = "Love"
+const val HELLO = "Hello"
 
 class ElyeProjectActivity : AppCompatActivity() {
 
-    @Inject lateinit var info: Info
+    @Inject @field:Named(LOVE) lateinit var infoLove: Info
+    @Inject @field:Named(HELLO) lateinit var infoHello: Info
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_elye_project)
         DaggerMagicBox.create().poke(this)
-        text_view.text = info.text
+        text_view.text = "${infoLove.text} ${infoHello.text}"
     }
 }
 
