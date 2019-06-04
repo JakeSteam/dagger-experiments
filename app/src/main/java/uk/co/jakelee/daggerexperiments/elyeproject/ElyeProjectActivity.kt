78ed0dc206dbd5b3870f4ef383214fc759a9adbf
Jake Lee <github@jakelee.co.uk>
2019-06-04 13:19:39 +0100

    First part of tutorial

--- app/src/main/java/uk/co/jakelee/daggerexperiments/elyeproject/ElyeProjectActivity.kt
+++ app/src/main/java/uk/co/jakelee/daggerexperiments/elyeproject/ElyeProjectActivity.kt
@@ -2,21 +2,28 @@ package uk.co.jakelee.daggerexperiments.elyeproject;
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import dagger.Component
 import kotlinx.android.synthetic.main.activity_elye_project.*
 import uk.co.jakelee.daggerexperiments.R
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class ElyeProjectActivity : AppCompatActivity() {
 
-        val info = Info()
+    @Inject lateinit var info: Info
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-                super.onCreate(savedInstanceState)
-                setContentView(R.layout.activity_elye_project)
-
-                text_view.text = info.text
-        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_elye_project)
+        DaggerMagicBox.create().poke(this)
+        text_view.text = info.text
+    }
 }
 
-class Info  {
-        val text = "Hello Dagger 2"
+class Info @Inject constructor() {
+    val text = "Hello Dagger 2"
+}
+
+@Component
+interface MagicBox {
+    fun poke(app: ElyeProjectActivity)
 }
