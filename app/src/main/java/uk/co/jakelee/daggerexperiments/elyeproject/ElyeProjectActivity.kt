7af1e1b005216a5b6bcf995a68f414e04273e4e8
Jake Lee <github@jakelee.co.uk>
2019-06-04 13:36:19 +0100

    Third part of tutorial

--- app/src/main/java/uk/co/jakelee/daggerexperiments/elyeproject/ElyeProjectActivity.kt
+++ app/src/main/java/uk/co/jakelee/daggerexperiments/elyeproject/ElyeProjectActivity.kt
@@ -3,18 +3,21 @@ package uk.co.jakelee.daggerexperiments.elyeproject;
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import dagger.Component
+import dagger.Module
+import dagger.Provides
 import kotlinx.android.synthetic.main.activity_elye_project.*
 import uk.co.jakelee.daggerexperiments.R
 import javax.inject.Inject
 import javax.inject.Named
+import javax.inject.Qualifier
 
 const val LOVE = "Love"
 const val HELLO = "Hello"
 
 class ElyeProjectActivity : AppCompatActivity() {
 
-    @Inject @field:Named(LOVE) lateinit var infoLove: Info
-    @Inject @field:Named(HELLO) lateinit var infoHello: Info
+    @Inject @field:Choose(LOVE) lateinit var infoLove: Info
+    @Inject @field:Choose(HELLO) lateinit var infoHello: Info
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +27,29 @@ class ElyeProjectActivity : AppCompatActivity() {
     }
 }
 
+@Module
+class Bag {
+    @Provides
+    @Named(LOVE)
+    open fun sayLoveDagger2(): Info {
+        return Info("Love Dagger 2")
+    }
+
+    @Provides
+    @Named(HELLO)
+    open fun sayHelloDagger2(): Info {
+        return Info("Hello Dagger 2")
+    }
+}
+
 class Info(val text: String)
 
 @Component(modules = [Bag::class])
 interface MagicBox {
     fun poke(app: ElyeProjectActivity)
 }
+
+@Qualifier
+@MustBeDocumented
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class Choose(val value: String = "")
